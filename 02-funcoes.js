1bda4457096d232694fabc60a97a5080c99845b7
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-11-21 10:56:28 -0300

    Arrow Function e exercicio

--- 02-funcoes.js
+++ 02-funcoes.js
@@ -17,3 +17,30 @@ function exemplo2(){
 }
 
 exemplo2()
+
+/* Exercícios
+1) Crie uma função (qualquer sintaxe) que receba dois valores numéricos, calcule a diferença entre eles e retorne o resultado.
+
+2) Chame esta função duas vezes passando valores diferentes e guardando-os em variáveis de escopo global.
+
+3) Mostre os valores processados pela função no terminal
+*/
+
+function exercicio(valor1,valor2){
+    return Math.abs(valor1 - valor2)
+}
+
+let resultado1 = exercicio(10,5)
+let resultado2 = exercicio(10,20)
+let resultado3 = exercicio(66,120)
+
+console.log(resultado1)
+console.log(resultado2)
+console.log(resultado3)
+
+/* Sintaxe Arrow Function */
+const exemplo3 = () => {
+    console.log("Arrow Function!")
+}
+
+exemplo3()
