9166e4ba2e40ecc5c3d615b3bff4821f64b7c61f
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-11-22 08:52:21 -0300

    Create 03-template-literal.js

---
+++ 03-template-literal.js
@@ -0,0 +1,13 @@
+// 03-template-literal.js
+
+const nome = "Chapolin"
+const sobrenome = "Colorado"
+let idade = 25
+let cidade = "São Paulo"
+let estado =  "SP"
+
+/* Concatenação tradicional usandor operador +
+ Meu nome é Chapoli Colorado, tenho 25 anos e moro atualmente na cidade de Sâo Paulo - SP. */
+let mensagem = "Meu nome é "+nome+" "+sobrenome+",tenho "+idade+" anos e moro atualmente na cidade de "+cidade+" - "+estado+"."
+
+console.log(mensagem);
