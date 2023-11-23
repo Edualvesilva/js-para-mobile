ea0996281de885cb516a1d859c8b0db5f65d26ee
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-11-23 10:47:36 -0300

    Modulo curso

---
+++ 08-map.js
@@ -0,0 +1,10 @@
+/* map (mapear - transforma em outra coisa) 
+Passa por elementos de um array e realiza operações em cada um deles atráves de uma função, gerando um novo array.
+*/
+
+// Exemplo 1
+const numeros = [10,20,5,12,55,50]
+const numerosDobrados = numeros.map(numero => numero * 2)
+console.log(numerosDobrados);
+
+
