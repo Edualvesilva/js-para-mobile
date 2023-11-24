3d56b4dc6de9a1d871f4b237a4e36c03db1f9c73
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-11-24 11:17:55 -0300

    Update 10-reduce.js

--- 10-reduce.js
+++ 10-reduce.js
@@ -1,3 +1,4 @@
+import cursos from "./modulos/cursos.js"
 /* reduce 
 Passa por elementos de um array (usando uma função callback com parâmetros, e um parâmetro com valor inicial) e retorna um único valor/resultado. Pode ser útil para operações matemáticas que geram um único resultado. */
 
@@ -10,7 +11,20 @@ console.log(total);
 
 // Sem reduce
 let acumuladora = 0
-for( ){
-
+for(let i = 0;i < valores.length;i++){
+   
+   acumuladora = acumuladora+ valores[i]
 }
+
+console.log(acumuladora);
+
+console.log("---------- Exercicio");
+
+/* Exercicio 1: */
+const resultado = cursos.reduce((acumulador,curso)=> acumulador+ curso.preco,0)
+console.log(resultado);
+
+/* Exercicio 2: faça a soma dos preços dos cursos de front e back-end */
+
+
 
