556fc00bd0f19c014888aeb9c23d8e582d1f239d
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-11-24 09:38:10 -0300

    Exercicio Resolvido

--- 09-filter.js
+++ 09-filter.js
@@ -1,3 +1,4 @@
+import cursos from "./modulos/cursos.js"
 /* filter (filtro)
 Passa por elementos de um array (usando uma função callback) e
 retorna valores de acordo com uma ou mais condições gerando um novo array. */
@@ -12,8 +13,29 @@ console.log(vendas);
 console.log(vendasAcimaDaMedia);
 
 // Exemplo 2
+// Filtrando alunos que começam com "M"
+// Obs.: faz difereça maiúsculas e minúsculas
 const alunos = ["Nicolas","Eliel","André","Kaue","Barbosa","Aline","Melissa","Marina"]
 const resultados =  alunos.filter((aluno)=> {return aluno.startsWith("M")} )
 
 console.log(alunos);
 console.log(resultados);
+console.log("------------");
+
+/* Filtrando cursos */
+
+// Filtrando por cursos da categoria Design
+const cursoDesign= cursos.filter((curso)=> curso.categoria == "Design" )
+console.log(cursos);
+console.log(cursoDesign);
+
+// Filtrando por cursos que NÃO SÃO da categoria Design
+const cursosMenosDesign= cursos.filter((curso)=> curso.categoria !== "Design" )
+console.log(cursosMenosDesign);
+
+/* Exercicio: Gere um novo arrray com os cursos que atendem os seguintes critérios:
+- Categorias Front-End e Mobile
+- Preços acima de 600*/
+
+const criterioso = cursos.filter(curso =>curso.preco >=600 && curso.categoria === "Front-End" || curso.categoria === "Mobile")
+console.log(criterioso);
